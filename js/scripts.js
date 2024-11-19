183c9ccefce9b4cd0a0e7452e7bff9c4bacc6c27
sebastian.larranaga <sebastian.larranaga@uth.ca>
2024-11-19 16:41:03 -0700

    3 box changes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+console.log("later code");
+
+$("#num-19").append("<h2>World</h2>")
+$("#num-19").append("<h2>Toilet Day</h2>")
+$("#num-19").css("background-image", "url(imgs/wtd.webp)")
+$("#num-19").css("background-size", "cover")
+$("#num-19").css("color", "white")
+
+$("#num-20").addClass("wcd");
+$("#num-20").append("<h2>World Children's Day</h2>")
+
+$("#num-21").addClass("wtd");
+$("#num-21").append("<h2>World Television Day</h2>")
+
+$("#num-22, #num-23, #num-24").remove();
+
+$("#num-25").addClass("idevaw");
