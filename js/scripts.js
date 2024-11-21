94985be85b9732f528ba6bf42bf436e411f2f58e
sebastian.larranaga <sebastian.larranaga@uth.ca>
2024-11-21 16:37:36 -0700

    lots of changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,41 @@ console.log("Hello, World!");
 
 console.log("later code");
 
-$("#num-19").append("<h2>World</h2>")
-$("#num-19").append("<h2>Toilet Day</h2>")
-$("#num-19").css("background-image", "url(imgs/wtd.webp)")
-$("#num-19").css("background-size", "cover")
-$("#num-19").css("color", "white")
+$("#num-19").on("click", function () {
+    console.log("You clicked on the 19th day of the month");
+    $(this).append("<h2>World</h2>")
+    $(this).append("<h2>Toilet Day</h2>")
+    $(this).css("background-image", "url(imgs/wtd.webp)")
+    $(this).css("background-size", "cover")
+    $(this).css("color", "white")
+    $(this).unbind("click");
+});
 
-$("#num-20").addClass("wcd");
-$("#num-20").append("<h2>World Children's Day</h2>")
+$("#num-20").on("dblclick", function () {
+    $(this).addClass("wcd");
+    $(this).append("<h2>World Children's Day</h2>")
+    $(this).unbind("dblclick");
+});
 
 $("#num-21").addClass("wtd");
-$("#num-21").append("<h2>World Television Day</h2>")
 
-$("#num-22, #num-23, #num-24").remove();
+$(".hide-days").on("click", function () {
+    console.log("Clicked on title");
+    $("#num-22, #num-23, #num-24").fadeToggle(2000)
+});
+
+$(document).on("keypress", function (e) {
+    console.log("You pressed " + e.key);
+    if (e.key == "v") {
+        $("#num-21").html("November 21");
+        $("#num-21").append("<h2>World Television Day</h2>")
+    }
+})
+
+
 
 $("#num-25").addClass("idevaw");
+$("#num-25").append("<h2>International Day for the Elimination of Violence agaist Women</h2>")
+
+$("#num-26").addClass("wstd");
+$("#num-26").append("<h2>World Sustainable Transport Day</h2>")
